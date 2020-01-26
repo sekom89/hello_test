9d5cb042667013d2089973205eb29cb749742a55
DongSoon Kim <sekom89@gmail.com>
2020-01-26 12:50:54 +0900

    테스트 내용 입니다.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        //
+        // TODO: Add constructor logic here
+        //
+
+        int a = 10;
+        var test = a;
+	}
+}
